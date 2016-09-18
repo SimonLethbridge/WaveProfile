2a039cf42704b6154f13f2702b09f66e37e874d8
Simon Lethbridge <simonlethbridge@Simons-iMac-2.local>
2016-09-18 14:21:07 +0100

    Working
    Commit with redacted history for GitHub.

--- WaveProfile/GameViewController.swift
+++ WaveProfile/GameViewController.swift
@@ -12,53 +12,48 @@ import MetalKit
 let MaxBuffers = 3
 let ConstantBufferSize = 1024*1024
 
-let vertexData:[Float] =
-[
-    -1.0, -1.0, 0.0, 1.0,
-    -1.0,  1.0, 0.0, 1.0,
-    1.0, -1.0, 0.0, 1.0,
-    
-    1.0, -1.0, 0.0, 1.0,
-    -1.0,  1.0, 0.0, 1.0,
-    1.0,  1.0, 0.0, 1.0,
-    
-    -0.0, 0.25, 0.0, 1.0,
-    -0.25, -0.25, 0.0, 1.0,
-    0.25, -0.25, 0.0, 1.0
-]
 
-let vertexColorData:[Float] =
-[
-    0.0, 0.0, 1.0, 1.0,
-    0.0, 0.0, 1.0, 1.0,
-    0.0, 0.0, 1.0, 1.0,
-    
-    0.0, 0.0, 1.0, 1.0,
-    0.0, 0.0, 1.0, 1.0,
-    0.0, 0.0, 1.0, 1.0,
-    
-    0.0, 0.0, 1.0, 1.0,
-    0.0, 1.0, 0.0, 1.0,
-    1.0, 0.0, 0.0, 1.0
-]
+
+struct ProfileCoord
+{
+    var x: Float = 0
+    var y: Float = 0
+};
+
+struct WaveParams
+{
+    var Qbase :     Float = 0.0
+    var amplitude : Float = 0.0
+    var height :    Float = 0.0
+};
+
+struct Constants 
+{
+    var colour = vector_float4(1.0, 1.0, 1.0, 1.0)
+    var offset = vector_float4(0.0, 0.0, 0.0, 1.0)
+}
+
+
 
 class GameViewController: NSViewController, MTKViewDelegate {
     
     var device: MTLDevice! = nil
     
     var commandQueue: MTLCommandQueue! = nil
+    var commandQueueKernel: MTLCommandQueue! = nil
     var pipelineState: MTLRenderPipelineState! = nil
-    var vertexBuffer: MTLBuffer! = nil
-    var vertexColorBuffer: MTLBuffer! = nil
+    var kernelPipeLineState: MTLComputePipelineState! = nil
+    var vertexIn:       MTLBuffer! = nil
+    var indexBuffer:    MTLBuffer! = nil
+    var waveProfileBuffers:     [MTLBuffer]! = nil
+    var wpParamsBuffer: MTLBuffer! = nil
+    var vertexCount: Int = 0
+    var indexCount: Int = 0
+    var waveProfileCount: Int = 0
     
     let inflightSemaphore = dispatch_semaphore_create(MaxBuffers)
     var bufferIndex = 0
     
-    // offsets used in animation
-    var xOffset:[Float] = [ -1.0, 1.0, -1.0 ]
-    var yOffset:[Float] = [ 1.0, 0.0, -1.0 ]
-    var xDelta:[Float] = [ 0.002, -0.001, 0.003 ]
-    var yDelta:[Float] = [ 0.001,  0.002, -0.001 ]
 
     override func viewDidLoad() {
         
@@ -78,6 +73,16 @@ class GameViewController: NSViewController, MTKViewDelegate {
         view.sampleCount = 4
         
         loadAssets()
+//        (vertexIn, vertexCount, indexBuffer, indexCount, waveProfileBuffers, wpParamsBuffer, waveProfileCount) = createProfileBuffers(device)
+        var profile_buffers = createProfileBuffers(device)
+        vertexIn         = profile_buffers.vertexIn
+        vertexCount      = profile_buffers.vertexCount
+        indexBuffer      = profile_buffers.indexBuffer
+        indexCount       = profile_buffers.indexCount
+        waveProfileBuffers        = profile_buffers.waveProfileBuffers
+        wpParamsBuffer   = profile_buffers.wpParamsBuffer
+        waveProfileCount = profile_buffers.waveProfileCount
+        
     }
     
     func loadAssets() {
@@ -86,6 +91,9 @@ class GameViewController: NSViewController, MTKViewDelegate {
         let view = self.view as! MTKView
         commandQueue = device.newCommandQueue()
         commandQueue.label = "main command queue"
+        
+        commandQueueKernel = device.newCommandQueue()
+        commandQueueKernel.label = "kernel command queue"
         
         let defaultLibrary = device.newDefaultLibrary()!
         let fragmentProgram = defaultLibrary.newFunctionWithName("passThroughFragment")!
@@ -102,91 +110,191 @@ class GameViewController: NSViewController, MTKViewDelegate {
         } catch let error {
             print("Failed to create pipeline state, error \(error)")
         }
-        
-        // generate a large enough buffer to allow streaming vertices for 3 semaphore controlled frames
-        vertexBuffer = device.newBufferWithLength(ConstantBufferSize, options: [])
-        vertexBuffer.label = "vertices"
-        
-        let vertexColorSize = vertexData.count * sizeofValue(vertexColorData[0])
-        vertexColorBuffer = device.newBufferWithBytes(vertexColorData, length: vertexColorSize, options: [])
-        vertexColorBuffer.label = "colors"
-    }
-    
-    func update() {
-        
-        // vData is pointer to the MTLBuffer's Float data contents
-        let pData = vertexBuffer.contents()
-        let vData = UnsafeMutablePointer<Float>(pData + 256*bufferIndex)
-        
-        // reset the vertices to default before adding animated offsets
-        vData.initializeFrom(vertexData)
-        
-        // Animate triangle offsets
-        let lastTriVertex = 24
-        let vertexSize = 4
-        for j in 0..<MaxBuffers {
-            // update the animation offsets
-            xOffset[j] += xDelta[j]
-            
-            if(xOffset[j] >= 1.0 || xOffset[j] <= -1.0) {
-                xDelta[j] = -xDelta[j]
-                xOffset[j] += xDelta[j]
-            }
-            
-            yOffset[j] += yDelta[j]
-            
-            if(yOffset[j] >= 1.0 || yOffset[j] <= -1.0) {
-                yDelta[j] = -yDelta[j]
-                yOffset[j] += yDelta[j]
-            }
-            
-            // Update last triangle position with updated animated offsets
-            let pos = lastTriVertex + j*vertexSize
-            vData[pos] = xOffset[j]
-            vData[pos+1] = yOffset[j]
-        }
-    }
-    
-    func drawInMTKView(view: MTKView) {
-        
-        // use semaphore to encode 3 frames ahead
-        dispatch_semaphore_wait(inflightSemaphore, DISPATCH_TIME_FOREVER)
-        
-        self.update()
-        
-        let commandBuffer = commandQueue.commandBuffer()
-        commandBuffer.label = "Frame command buffer"
-        
-        // use completion handler to signal the semaphore when this frame is completed allowing the encoding of the next frame to proceed
-        // use capture list to avoid any retain cycles if the command buffer gets retained anywhere besides this stack frame
-        commandBuffer.addCompletedHandler{ [weak self] commandBuffer in
-            if let strongSelf = self {
-                dispatch_semaphore_signal(strongSelf.inflightSemaphore)
-            }
-            return
-        }
-        
-        if let renderPassDescriptor = view.currentRenderPassDescriptor, currentDrawable = view.currentDrawable
-        {
-            let renderEncoder = commandBuffer.renderCommandEncoderWithDescriptor(renderPassDescriptor)
-            renderEncoder.label = "render encoder"
-            
-            renderEncoder.pushDebugGroup("draw morphing triangle")
-            renderEncoder.setRenderPipelineState(pipelineState)
-            renderEncoder.setVertexBuffer(vertexBuffer, offset: 256*bufferIndex, atIndex: 0)
-            renderEncoder.setVertexBuffer(vertexColorBuffer, offset:0 , atIndex: 1)
-            renderEncoder.drawPrimitives(.Triangle, vertexStart: 0, vertexCount: 9, instanceCount: 1)
-            
-            renderEncoder.popDebugGroup()
-            renderEncoder.endEncoding()
                 
-            commandBuffer.presentDrawable(currentDrawable)
+        if let kernelProfileFunction = defaultLibrary.newFunctionWithName("profileKernel")
+        {
+            // let kernelPipelineStateDescriptor = MTLRenderPipelineDescriptor()
+            kernelPipeLineState = try? device.newComputePipelineStateWithFunction(kernelProfileFunction)
+        }
+    }
+ 
+    func createWaveProfileInputBuffer(device : MTLDevice) -> (vertexIn: MTLBuffer, vertexCount:Int, indexBuffer: MTLBuffer, indexCount:Int)
+    {
+        var vector : [ProfileCoord] = [ ]
+        var indexes : [UInt16] = []
+        let NUM_POINTS = 512
+        
+        vector.reserveCapacity(NUM_POINTS)
+        for i in 0..<NUM_POINTS
+        {
+            let xx = 2.0 * (Float(i) / Float(NUM_POINTS-1)) - 1.0
+            vector +=  [ ProfileCoord(x:xx, y:1.0), ProfileCoord(x:xx, y: -1.0) ]
+            
+            if i > 0
+            {
+                let a = UInt16(2*(i-1))
+                let b = a+1
+                let c = b+1
+                let d = c+1
+                indexes += [ a, c, b, c, d, b]
+            }
         }
         
-        // bufferIndex matches the current semaphore controled frame index to ensure writing occurs at the correct region in the vertex buffer
-        bufferIndex = (bufferIndex + 1) % MaxBuffers
+        let bufferLengthBytes = vector.count*sizeof(ProfileCoord)
+        var inBuffer = device.newBufferWithBytes(vector, length: bufferLengthBytes, options: [])
+        inBuffer.label = "in"
         
-        commandBuffer.commit()
+        var indexBuffer = device.newBufferWithBytes(indexes, length: indexes.count * sizeof(UInt16), options: [])
+        indexBuffer.label = "index buffer"
+        
+        return (vertexIn: inBuffer, vertexCount:vector.count, indexBuffer: indexBuffer, indexCount: indexes.count)
+    }
+    
+    func createProfileBuffers(device : MTLDevice) -> ( 
+        vertexIn: MTLBuffer,
+        vertexCount : Int,
+        indexBuffer: MTLBuffer,
+        indexCount : Int, 
+        waveProfileBuffers: [MTLBuffer],  
+        wpParamsBuffer: MTLBuffer,
+        waveProfileCount: Int)
+    {
+        let (vertexIn, vertexCount, indexBuffer, indexCount) = createWaveProfileInputBuffer(device)
+        let qValues : [Float] = [0.0, 0.2, 0.4, 0.6]
+//        var wp : [WaveParams] = []
+        let paramsBufferFloatsPerSet = 256 / sizeof(Float)
+        var waveParametersFloats = [Float](count:paramsBufferFloatsPerSet*qValues.count, repeatedValue:0.0)
+        var waveProfileBuffers : [MTLBuffer] = []
+        let bufferLengthBytes = vertexCount*sizeof(ProfileCoord)
+                
+        var i = 0
+        for q in qValues
+        {
+            let wp = WaveParams(Qbase: q, amplitude: 0.2, height: -0.5+1.6*q)
+            waveParametersFloats[paramsBufferFloatsPerSet*i + 0] = wp.Qbase
+            waveParametersFloats[paramsBufferFloatsPerSet*i + 1] = wp.amplitude
+            waveParametersFloats[paramsBufferFloatsPerSet*i + 2] = wp.height 
+            i += 1
+
+            waveProfileBuffers += [ device.newBufferWithLength(bufferLengthBytes, options: []) ]            
+        }
+        var waveProfileParamsBuffer = device.newBufferWithBytes(waveParametersFloats, length: sizeof(Float)*waveParametersFloats.count, options: [])
+        waveProfileParamsBuffer.label = "waveParameters"
+        
+        return (vertexIn, vertexCount, indexBuffer, indexCount, waveProfileBuffers, waveProfileParamsBuffer, qValues.count)
+    }
+   
+    func drawInMTKView(view: MTKView) {
+        do
+        {
+            //
+            // Compute wave profiles and store results in waveProfileBuffers
+            //
+            let commandBuffer = commandQueue.commandBuffer()
+            commandBuffer.label = "Compute command buffer"
+            
+            if let kpls = kernelPipeLineState
+            {
+                let waveProfileParamsBufferBytesPerParam = wpParamsBuffer.length / waveProfileBuffers.count
+                assert(waveProfileParamsBufferBytesPerParam==256, "Bad paramater buffer alignment")
+                var i = 0
+                
+                for waveProfileBuffer in waveProfileBuffers
+                {
+                    let commandEncoder: MTLComputeCommandEncoder = commandBuffer.computeCommandEncoder()
+                    
+                    commandEncoder.setComputePipelineState(kpls)
+                    
+                    
+                    commandEncoder.setBuffer(vertexIn,          offset:0, atIndex: 0)
+                    commandEncoder.setBuffer(waveProfileBuffer, offset:0, atIndex: 1)
+                    commandEncoder.setBuffer(wpParamsBuffer,    offset:256*i, atIndex: 2)
+                    i += 1
+                
+                        // A one dimensional thread group Swift to pass Metal a one dimensional array
+                    let threadGroupWidth = 4
+                    let threadgroupsPerGrid = MTLSize(width: threadGroupWidth, height:1, depth:1)
+                    let threadsPerThreadgroup = MTLSize(width: vertexCount/threadGroupWidth, height:1, depth:1)
+                    
+                    commandEncoder.dispatchThreadgroups(threadgroupsPerGrid, threadsPerThreadgroup: threadsPerThreadgroup)
+                    
+                    commandEncoder.endEncoding()
+                }
+                
+                commandBuffer.commit()
+                commandBuffer.waitUntilCompleted()
+                let status = commandBuffer.status
+                print("status=\(commandBuffer.status)")
+                
+                
+                //
+                // Render wave profiles to screen
+                //
+                let commandBufferRender = commandQueue.commandBuffer()
+                commandBufferRender.label = "Frame command buffer"
+                
+                if var renderPassDescriptor = view.currentRenderPassDescriptor, currentDrawable = view.currentDrawable
+                {
+                    var loadActionActual = MTLLoadAction.Clear
+                    var renderPassDescriptorActual = renderPassDescriptor
+                    let redactedwaveProfileBuffers = waveProfileBuffers.reverse()
+                    var constants = Constants()
+                    for waveProfileBuffer in redactedwaveProfileBuffers
+                    {     
+                        // debug stuff          
+                        do
+                        {
+                            let bufferLengthBytes = waveProfileBuffer.length
+                            var data = NSData(bytesNoCopy: waveProfileBuffer.contents(),length: bufferLengthBytes, freeWhenDone: false)
+                            var datain = NSData(bytesNoCopy: vertexIn.contents(),length: bufferLengthBytes, freeWhenDone: false)
+                            var vector_out   : [ProfileCoord] = Array(count: vertexCount, repeatedValue:  ProfileCoord(x:0.0, y:0.0))
+                            var vector_inout : [ProfileCoord] = Array(count: vertexCount, repeatedValue:  ProfileCoord(x:0.0, y:0.0))
+
+                            data.getBytes(&vector_out, length:bufferLengthBytes)
+                            datain.getBytes(&vector_inout, length:bufferLengthBytes)
+                        }
+                        
+                        // Render stuff
+                        renderPassDescriptorActual.colorAttachments[0].loadAction = loadActionActual
+//                        renderPassDescriptorActual.colorAttachments[0].storeAction = .Store
+                        let renderEncoder = commandBufferRender.renderCommandEncoderWithDescriptor(renderPassDescriptorActual)
+                        renderEncoder.label = "render encoder"
+                        
+                        renderEncoder.pushDebugGroup("draw wave profile")
+                        renderEncoder.setCullMode(.None)
+                        renderEncoder.setFrontFacingWinding(.Clockwise)
+                        
+                        renderEncoder.setRenderPipelineState(pipelineState)
+                        
+                        // Bind the uniform buffer so we can read our model-view-projection matrix in the shader.
+                        constants.colour[0] *= 0.8
+                        constants.colour[1] *= 0.8
+                        constants.offset[3] -= 0.0
+                        renderEncoder.setVertexBytes(&constants, length: sizeof(Constants), atIndex: 1)
+                        
+                        
+                        renderEncoder.setVertexBuffer(waveProfileBuffer, offset: 0, atIndex: 0)
+                        renderEncoder.drawIndexedPrimitives(.Triangle,
+                                                            indexCount: indexCount,
+                                                            indexType: .UInt16,
+                                                            indexBuffer: indexBuffer,
+                                                            indexBufferOffset: 0)
+                        
+                        renderEncoder.popDebugGroup()
+                        renderEncoder.endEncoding()
+                        loadActionActual = .Load
+                            
+                    } // for wave profile buffer
+                                    
+                    commandBufferRender.presentDrawable(currentDrawable)
+                    commandBufferRender.commit()
+                    commandBufferRender.waitUntilCompleted()
+                } // if render pass descriptor
+                
+            }
+        }
+        
+        
     }
     
     
--- WaveProfile/Shaders.metal
+++ WaveProfile/Shaders.metal
@@ -13,22 +13,67 @@ using namespace metal;
 struct VertexInOut
 {
     float4  position [[position]];
-    float4  color;
+    float4  colour;
 };
 
-vertex VertexInOut passThroughVertex(uint vid [[ vertex_id ]],
-                                     constant packed_float4* position  [[ buffer(0) ]],
-                                     constant packed_float4* color    [[ buffer(1) ]])
+struct ProfileCoord
+{
+    float x,y;
+};
+
+struct Constants
+{
+    float4 colour;
+    float4 offset;
+};
+
+struct WaveParams
+{
+    float Qbase, amplitude, height;
+};
+
+kernel void profileKernel (
+    const device ProfileCoord *meshCoordsIn  [[ buffer(0) ]],
+          device ProfileCoord *meshCoordsOut [[ buffer(1) ]],
+    constant WaveParams *waveParams             [[ buffer(2) ]],
+          uint id [[thread_position_in_grid]]
+    )
+{
+    if (meshCoordsIn[id].y > -1.0f)
+    {
+        const float Q  = waveParams->Qbase * 0.5f;
+        const float phase = meshCoordsIn[id].x * 10.0f;
+        
+        ProfileCoord tmp;
+        tmp.x = meshCoordsIn[id].x + Q * waveParams->amplitude *  cos (phase);
+        tmp.y = meshCoordsIn[id].y*waveParams->height +     waveParams->amplitude * (sin (phase) - 1.0f);
+        meshCoordsOut[id]=tmp;
+    }
+    else
+    {
+        meshCoordsOut[id]=meshCoordsIn[id];
+    }
+}
+
+vertex VertexInOut passThroughVertex(uint vid                         [[ vertex_id ]],
+                                     constant ProfileCoord* position  [[ buffer(0) ]],
+                                     constant Constants &constants    [[ buffer(1) ]])
 {
     VertexInOut outVertex;
     
-    outVertex.position = position[vid];
-    outVertex.color    = color[vid];
+    outVertex.position.x = position[vid].x;
+    outVertex.position.y = position[vid].y;
+    outVertex.position.z = constants.offset[3];
+    outVertex.position.w = 1.0f;
+
+//    outVertex.color    = float4(0.5f, 0.5f, 1.0f, 1.0f);
+    outVertex.colour    = float4(constants.colour);
     
     return outVertex;
 };
 
 fragment half4 passThroughFragment(VertexInOut inFrag [[stage_in]])
 {
-    return half4(inFrag.color);
+    return half4(inFrag.colour);
 };
+
